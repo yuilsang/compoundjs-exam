cf22ab256ed5e929d33302ea23819bec4d3d4c87
ilzzang <yuilsang@gmail.com>
2013-03-29 22:06:03 +0900

    home 작업

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,11 @@
 exports.routes = function (map) {
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
-    map.all(':controller/:action');
-    map.all(':controller/:action/:id');
+//    map.all(':controller/:action');
+//    map.all(':controller/:action/:id');
+
+    // HOME
+    map.get("/", "home#index");
+
+    //
 };
