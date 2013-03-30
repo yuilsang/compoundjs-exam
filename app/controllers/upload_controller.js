fd9a74ada0bb929d77ada3127f9e1031e16bf385
ilzzang <yuilsang@gmail.com>
2013-03-30 21:54:32 +0900

    #1 업로드 기능 추가하기

--- app/controllers/upload_controller.js
+++ app/controllers/upload_controller.js
@@ -6,8 +6,51 @@ action("index", function () {
     });
 });
 
+action("api_uploadedfilelist", function() {
+    var fs = require("fs");
+    var async = require("async");
+
+
+    async.series([function(next) {
+       // read file
+       fs.readdir("files/", function(err, files) {
+           if(err) {
+               next(err);
+               return;
+           }
+
+           next(null, files);
+       });
+    }], function(err, datas) {
+        // fail
+        if(err) {
+            if(req.params.format == "html") {
+                flash('error', 'upload fail');
+                redirect("/upload");
+            } else {
+                send({
+                    code: 1
+                });
+            }
+            return;
+        }
+
+        // success
+        if(req.params.format == "html") {
+            flash('info', 'upload ok');
+            redirect("/upload");
+        } else {
+            send({
+                code: 0,
+                files: datas[0]
+            });
+        }
+    });
+});
+
 action("api_upload", function() {
     var fs = require("fs");
+    var async = require("async");
     var type = req.body.type;
 
     var file_tmp = req.files.file.path;
@@ -15,19 +58,38 @@ action("api_upload", function() {
     var file_name = req.files.file.name;
     var file_savepath = "files/" + file_name;
 
-    fs.rename( file_tmp, file_savepath, function(err){
-        if(err) console.log(err);
-        console.log("moved");
-        return;
+    async.series([function(next) {
+        // file upload
+        fs.rename( file_tmp, file_savepath, function(err){
+            if(err) next(err);
+
+            next(null);
+
+            return;
+        });
+    }], function(err, datas) {
+        // fail
+        if(err) {
+            if(req.params.format == "html") {
+                flash('error', 'upload fail');
+                redirect("/upload");
+            } else {
+                send({
+                    code: 1
+                });
+            }
+            return;
+        }
+
+        // success
+        if(req.params.format == "html") {
+            flash('info', 'upload ok');
+            redirect("/upload");
+        } else {
+            send({
+                code: 0
+            });
+        }
     });
 
-
-    if(req.params.format == "html") {
-        flash('info', 'upload ok');
-        redirect("/upload");
-    } else {
-        send({
-            code: 0
-        });
-    }
 });
--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,21 @@
 exports.routes = function (map) {
 
+    // ================================
     // HOME
+    // ================================
     map.get("/", "home#index");
 
+    // ================================
     // UPLOAD
-    map.get("/upload", "upload#index"); // http://localhost:3000/upload
-    map.post("/api/upload.:format?", "upload#api_upload"); // http://localhost:3000/api/upload
+    // ================================
+
+    // http://localhost:3000/upload
+    map.get("/upload", "upload#index");
+
+    // http://localhost:3000/api/uploadedfilelist
+    map.get("/api/uploadedfilelist.:format?", "upload#api_uploadedfilelist");
+
+    // http://localhost:3000/api/upload
+    map.post("/api/upload.:format?", "upload#api_upload");
 
 };
