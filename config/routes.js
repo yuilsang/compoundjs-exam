b572f67e8d681af1036556601c96c210b661cd35
ilzzang <yuilsang@gmail.com>
2013-03-30 20:45:32 +0900

    Revert "#1 업로드 기능 구현"
    This reverts commit 6fd54f145a7ee4dccbb505ad6c17245b6c17f87c.

--- config/routes.js
+++ config/routes.js
@@ -1,21 +1,10 @@
 exports.routes = function (map) {
 
-    // ================================
     // HOME
-    // ================================
     map.get("/", "home#index");
 
-    // ================================
     // UPLOAD
-    // ================================
-
-    // http://localhost:3000/upload
-    map.get("/upload", "upload#index");
-
-    // http://localhost:3000/api/uploadedfilelist
-    map.get("/api/uploadedfilelist.:format?", "upload#api_uploadedfilelist");
-
-    // http://localhost:3000/api/upload
-    map.post("/api/upload.:format?", "upload#api_upload");
+    map.get("/upload", "upload#index"); // http://localhost:3000/upload
+    map.post("/api/upload.:format?", "upload#api_upload"); // http://localhost:3000/api/upload
 
 };
