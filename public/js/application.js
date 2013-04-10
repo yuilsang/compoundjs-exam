b34fc3187d9004be819cc49d8c4c20974b8e855b
ilzzang <yuilsang@gmail.com>
2013-04-10 21:32:21 +0900

    js commit

--- public/js/application.js
+++ public/js/application.js
@@ -1,9 +1,25 @@
-/*
-  Add your application's coffee-script code here
-*/
-
+require.config({
+    "baseUrl" : "/js",
+    "urlArgs" : "ver=" + Math.floor(new Date().getTime() / 1000),
+    "paths" : {
+        /*"case" : "../case"*/
+    }
+});
 
 (function() {
-
-
+    require(
+        [
+            "app/common/url",
+            "app/controllers/menu.controller",
+            "app/controllers/test.controller"
+        ],
+        function(
+            URL,
+            MenuController,
+            TestController
+            ) {
+                new MenuController();
+                new TestController();
+        }
+    );
 }).call(this);
