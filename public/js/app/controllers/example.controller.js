3e0d5229382fad83509eb3beace6d07aeef64be0
ilzzang <yuilsang@gmail.com>
2013-04-11 01:23:55 +0900

    #2 정리

--- public/js/app/controllers/example.controller.js
+++ public/js/app/controllers/example.controller.js
@@ -9,6 +9,7 @@ define(
     [
         "app/commons/controller",
         "app/commons/url",
+        "app/commons/flow",
         "app/commons/request",
         "app/views/request.example.view"
     ],
@@ -16,6 +17,7 @@ define(
     function(
         Controller,
         URL,
+        Flow,
         Request,
         RequestExampleView
         ) {
@@ -24,13 +26,32 @@ define(
         var ExampleController = Controller.$extend(/** @lends ExampleController.prototype */{
             $init: function() {
                 RequestExampleView.on("click", "._request", this.onclick.bind(this));
+                RequestExampleView.on("click", "._request_sync", this.onclick.bind(this));
             },
             onclick: function() {
                 this.respondSelector("._request", function() {
-                    Request.$("my").method("GET").url("/example/api/request").data({authenticity_token: $('meta[name=csrf-token]').attr('content') }).send(function() {
+                    Request.$("ExampleController").method("GET").url("/example/api/request").data({authenticity_token: $('meta[name=csrf-token]').attr('content') }).send(function() {
                         Request.done(function(r) {
                             console.log(r);
                         });
+                    });
+                }.bind(this));
+
+                this.respondSelector("._request_sync", function() {
+                    Flow.sync([function(next) {
+                        Request.$("ExampleController").method("GET").url("/example/api/request").data({authenticity_token: $('meta[name=csrf-token]').attr('content') }).send(function() {
+                            Request.done(function(r) {
+                                next(null, r);
+                            });
+                        });
+                    }, function(next) {
+                        Request.$("ExampleController").data({ data1: "23121321" }).send(function() {
+                            Request.done(function(r) {
+                                next(null, r);
+                            });
+                        });
+                    }], function(err, self) {
+                        console.log(err, self);
                     });
                 }.bind(this));
             }
